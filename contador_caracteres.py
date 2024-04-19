276903366355afc52f79aabb0f3583727184bc0e
victor cardozo <124733617+victorscardozo@users.noreply.github.com>
2024-04-19 14:24:32 -0300

    Add files via upload

--- contador_caracteres.py
+++ contador_caracteres.py
@@ -3,47 +3,70 @@ import customtkinter
 
 
 
+
+
+
+#Funcao para contar caracteres 
 def contar_caracteres():
 
-   
-    frase = entry.get()
+    #Pegar o valor inserido na textbox e atribuí-lo à variavel frase
+    frase = textbox.get(1.0, END) 
+
+    #Identificar o numero de caracteres total
     caracteres_com_espaco = len(frase)
-
-    print(frase)
-
-
-   # caracteres_sem_espaco = len(frase)- frase.count(" ")
-    #print(caracteres_sem_espaco)
+    #Identificar o numero de caracteres total - espaços
+    caracteres_sem_espaco = len(frase)- frase.count(" ")
 
 
 
-    texto_2["text"] = caracteres_com_espaco
+    #Substituir o Texto 2 pelo resultado 
+    texto_2["text"] = caracteres_com_espaco-1
 
+    #Substituir o texto 3 pelo resultado 
+    texto_3["text"] = caracteres_sem_espaco-1
+
+
+
+
+#Definir a janela 1
 janela = customtkinter.CTk()
 
-janela.title("Tkinter.com - CustomTkinter TextBox")
-janela.geometry('400x400')
+#Tilulo e dimensões da janela
+janela.title("Contador de Caracteres")
+janela.geometry('400x500')
 
 
-
-entry = Entry(janela, width=50, font=('Arial 10'))
-entry.grid(column=1, row=2, padx=30, pady=10)
-
-#textbox = customtkinter.CTkTextbox(janela)
-#textbox.pack(pady=20)
+#Definindo a caixa de texto e sua posiçao
+textbox = customtkinter.CTkTextbox(janela)
+textbox.grid(pady=20,padx=20, column=1, row=2, ipadx=70)
 
 
-
+#Definindo texto 1 e sua posição
 texto_1 = Label(janela, text = 'Insira abaixo o texto:')
 texto_1.grid(column=1, row=1, padx=30, pady=10)
 
 
-
+#Definindo Botão, sua posição e a função que executará
 botao = Button(janela, text='Contar caracteres', command=contar_caracteres)
 botao.grid(column=1, row=3, padx=30, pady=10)
 
 
-
+#Definindo texto 2 e sua posição
 texto_2 = Label(janela, text="")
-texto_2.grid(column=1, row=4, padx=30, pady=40)
+texto_2.grid(column=1, row=4, padx=30, pady=40, ipadx=15)
+
+
+#Definindo texto 3 e sua posição
+texto_3 = Label(janela, text="")
+texto_3.grid(column=1, row=5, padx=30, pady=40, ipadx=15)
+
+
+
+
+
+
+
+
+
+
 janela.mainloop()
